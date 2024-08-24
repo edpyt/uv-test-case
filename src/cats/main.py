622943c48079150f162f6a7841fecb88a7efeee8
edpyt <edrsa1321s@gmail.com>
2024-08-24 18:47:12 +0300

    add cats

--- src/cats/main.py
+++ src/cats/main.py
@@ -1,20 +1,33 @@
-from typing import Literal
+from pydantic import BaseModel, field_validator
+from quart import Quart
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-app = FastAPI()
+app = Quart(__name__)
 
 
 class Cat(BaseModel):
     name: str
-    age: int
+    age: float
+
+    @field_validator("name")
+    def name_title(cls, name: str) -> str:
+        return name.title()
 
 
-@app.get("/", response_model=list[Cat])
-def cats() -> list[dict[Literal["name", "age"], str | int]]:
-    return [
-        {"name": "Pushok", "age": 1},
-        {"name": "Martik", "age": 3},
-        {"name": "Maxwell", "age": 0},
-    ]
+@app.get("/")
+async def cats() -> list[dict]:
+    return list(
+        map(
+            lambda cat: cat.model_dump(),
+            map(
+                Cat.model_validate,
+                (
+                    {"name": "pushok", "age": 1},
+                    {"name": "MartiK", "age": 3},
+                    {"name": "MaxWELL", "age": 0},
+                ),
+            ),
+        )
+    )
+
+
+app.run()
