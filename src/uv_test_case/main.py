c6fd35f988e1aebd6ee7fc3e6d41f6f28a55513c
edpyt <edrsa1321s@gmail.com>
2024-08-29 08:00:47 +0300

    simple ui

--- src/uv_test_case/main.py
+++ src/uv_test_case/main.py
@@ -1,8 +1,9 @@
 import os
 from dataclasses import dataclass, field
 from functools import lru_cache
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from nicegui import app as nicegui_app
 from nicegui import ui
 
@@ -11,7 +12,7 @@ from nicegui import ui
 class Config:
     CARS: str = field(default_factory=lambda: os.getenv("CARS_API", default="http://localhost:8001"))
     CATS: str = field(default_factory=lambda: os.getenv("CATS_API", default="http://localhost:8002"))
-    DOGS: str = field(default_factory=lambda: os.getenv("DOGS_API", default="http://localhost:8001"))
+    DOGS: str = field(default_factory=lambda: os.getenv("DOGS_API", default="http://localhost:8003"))
 
 
 @lru_cache
@@ -21,11 +22,16 @@ def get_config() -> Config:
 
 def frontend_init(fastapi_app: FastAPI):
     @ui.page("/")
-    def show():
-        ui.label("UV Test Case!")
-
+    def show(config: Annotated[Config, Depends(get_config)]):
         ui.dark_mode().bind_value(nicegui_app.storage.user, "dark_mode")
         ui.checkbox("dark mode").bind_value(nicegui_app.storage.user, "dark_mode")
+
+        ui.label("UV Test Case!").style("font-size: 18px;")
+
+        with ui.grid():
+            ui.link("cars", config.CARS)
+            ui.link("cats", config.CATS)
+            ui.link("dogs", config.DOGS)
 
     ui.run_with(fastapi_app, mount_path="/", storage_secret="uv-testte")
 
